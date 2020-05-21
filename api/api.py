db4c73887fd5890b5aa2e998bfb4747d8e62f6ed
Naman-sopho <namantiwari.nt@gmail.com>
2020-05-21 18:18:36 -0400

    Implement real time response for user as they type.

--- api/api.py
+++ api/api.py
@@ -7,6 +7,7 @@ app = flask.Flask(__name__)
 
 
 initial_html = ' <form><input type="text" id="input-text" name="input-text"></form>\
+<h4 id="result"></h4>\
 <script src="https://code.jquery.com/jquery-3.5.1.js"\
     integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous">\
 </script>\
@@ -25,9 +26,12 @@ function doneTyping () {\
     console.log(\'done typing\');\
     var text = $("#input-text").val();\
     console.log(text);\
-    $.post(\'/analyze\', {\
-        "input-text": text\
-    });\
+    $.post(\'/analyze\',\
+        {"input-text": text},\
+        function(data) {\
+            $("#result").html(data["feedback"])\
+        }\
+    );\
 }\
 </script>'
 
@@ -38,6 +42,23 @@ def root():
 @app.route('/analyze', methods=['POST'])
 def analyze_text():
     input_text = request.form['input-text']
-    return input_text
+
+    # call to text analyzer like BERT
+    # assuming model returns JSON
+    result = '{"report": "56"}'
+    result_json = json.loads(result)
+    report = int(result_json['report'])
+    
+    feedback = str(report) + chr(37) + ' likely to be hate speech. '
+    if report > 70:
+        feedback += 'Please edit.'
+    elif report > 40:
+        feedback += 'Please consider editing before posting.'
+    else:
+        feedback += 'Good to go.'
+    
+    feedback_json = {"feedback": feedback}
+    feedback_json = jsonify(feedback_json)
+    return feedback_json
 
 app.run()
